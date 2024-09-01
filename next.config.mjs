f529216f732a5ad024040cfa730b0502e97bb205
TathagatChavada <chavadatathagat1234@gmail.com>
2024-09-01 16:59:23 +0530

    Deleted a file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
-      ignoreBuildErrors: true,
+      ignoreBuildErrors: true ,
     },
     eslint: {
       ignoreDuringBuilds: true,
