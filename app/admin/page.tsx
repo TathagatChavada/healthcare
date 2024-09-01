82d771453ca51d3b6cc02bb90d4266dd029aa10b
TathagatChavada <chavadatathagat1234@gmail.com>
2024-09-01 22:15:58 +0530

    changes git file

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { StatCard } from "@/components/StatCard";
-import { columns } from "@/components/table/columns";
+import { columns } from "@/components/table/columns"; 
 import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 
