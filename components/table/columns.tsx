4767870d83934bcb630658045a65e363ceff4520
TathagatChavada <chavadatathagat1234@gmail.com>
2024-09-02 00:46:52 +0530

    Doctor's photo is now visible

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -62,13 +62,13 @@ export const columns: ColumnDef<Appointment>[] = [
       return (
         <div className="flex items-center gap-3">
           <Image
-            src={doctor?.image!}
+            src={doctor!.image!}
             alt="doctor"
             width={100}
             height={100}
             className="size-8"
           />
-          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          <p className="whitespace-nowrap">Dr. {doctor!.name}</p>
         </div>
       );
     },
