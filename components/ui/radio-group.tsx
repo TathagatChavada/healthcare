441db28ce6b18efb981f4ed24b042ec40a507c40
TathagatChavada <chavadatathagat1234@gmail.com>
2024-09-03 15:29:57 +0530

    Website updated a little bit

--- components/ui/radio-group.tsx
+++ components/ui/radio-group.tsx
@@ -34,7 +34,7 @@ const RadioGroupItem = React.forwardRef<
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="size-2.5 fill-current text-current" />
+        <Circle className="size-2.5 fill-current text-current " />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
